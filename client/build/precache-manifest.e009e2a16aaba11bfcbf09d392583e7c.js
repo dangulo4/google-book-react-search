519fe129a8751f42d3bf1c66e795add87090d99c
Daniel Angulo <dangulo4@gmail.com>
2020-05-28 12:46:06 -0700

    gitignore

---
+++ client/build/precache-manifest.e009e2a16aaba11bfcbf09d392583e7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "a42b7bddd688fa916571",
+    "url": "/static/css/main.c8e4c0d4.chunk.css"
+  },
+  {
+    "revision": "a42b7bddd688fa916571",
+    "url": "/static/js/main.e0569cb8.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a400df5adf5bac22e5a6",
+    "url": "/static/js/2.d6838b31.chunk.js"
+  },
+  {
+    "revision": "13ce9f81cabab1472e72976a01b2a101",
+    "url": "/index.html"
+  }
+];
